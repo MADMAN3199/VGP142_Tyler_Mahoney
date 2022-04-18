ac2127da5c3842ecf57f1651179f2c65c1eeb087
MADMAN3199 <tylermahoney1999@hotmail.com>
2022-04-18 14:31:33 -0400

    Lab 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,117 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+    CharacterController controller;
+
+    [Header("Player Settings")]
+    [Space(2)]
+    [Tooltip("Speed value between 1 and 6")]
+    [Range(1.0f, 6.0f)]
     public float speed;
-    Rigidbody rb;
+    public float jumpSpeed;
+    public float rotationSpeed;
+    public float gravity;
+
+    [Header("Weapon Settings")]
+    public float projectileForce;
+    public Rigidbody projectilePrefab;
+    public Transform projectileSpawnPoint;
+
+    Vector3 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        try 
+        {
+            controller = GetComponent<CharacterController>();
+            controller.minMoveDistance = 0.0f;
+
+            if (speed <= 0) 
+            {
+                speed = 6.0f;
+            }
+            if (jumpSpeed <= 0)
+            {
+                jumpSpeed = 6.0f;
+            }
+            if (rotationSpeed <= 0)
+            {
+                rotationSpeed = 10.0f;
+            }
+            if (gravity <= 0)
+            {
+                gravity = 9.81f;
+            }
+
+            moveDirection = Vector3.zero;
+
+            if (projectileForce <= 0)
+            {
+                projectileForce = 10.0f;
+            }
+
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+        catch (UnassignedReferenceException e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+        finally
+        {
+            Debug.Log("Always Gets Called");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical"));
-        rb.velocity = moveDirection;
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            moveDirection *= speed;
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            if (Input.GetButtonDown("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.x = Input.GetAxis("Horizontal") * speed;
+            moveDirection.z = Input.GetAxis("Vertical") * speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+
+    void Fire()
+    {
+        Debug.Log("You may fire when ready");
+    }
+
+    [ContextMenu("Reset Stats")]
+    void ResetStats()
+    {
+        speed = 6.0f;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Finish")
-            Debug.Log("LoadNextLevel");
+            Debug.Log("Game End");
     }
 }
